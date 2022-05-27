e5cf6bce073fb864ce5dea6257a95f5fa44ae630
Rachel Bailey <rachel.ebailey@yahoo.com>
2022-05-26 21:49:46 -0400

    added drawer navigation

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import SearchBraids from './SearchBraids';
-import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
 import BraidInfo from './BraidInfoComponent';
 import Constants from 'expo-constants';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createAppContainer } from 'react-navigation';
 
 
 const BraidNavigator = createStackNavigator(
@@ -26,9 +28,37 @@ const BraidNavigator = createStackNavigator(
     }
 );
 
-const AppNavigator= createAppContainer(BraidNavigator);
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    }, 
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#AC7D88'
+            },
+            headerTintColor: '#F8ECD1',
+            headerTitleStyle: {
+                color: '#F8ECD1'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator},
+        BraidNavigator: { screen: BraidNavigator }
+    },
+    {
+        drawerBackgroundColor: '#85586F'
+    }
+)
+
+const AppNavigator= createAppContainer(MainNavigator);
 
 class Main extends Component {
+
     render() {
         return (
             <View 
